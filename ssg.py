8bee6eeaf8b7499afe1f3f92ea73fa2030406fba
Lucian Oană <98779636+0lucian@users.noreply.github.com>
2022-10-14 23:13:21 +0300

    testing Site class module

---
+++ ssg.py
@@ -0,0 +1,14 @@
+import typer
+from ssg.site import Site
+
+
+def main(source="content", dest="dist"):
+    config = {}
+    config[source] = "source"
+    config[dest] = "dest"
+
+    site = Site(**config)
+    Site.build()
+
+
+typer.run(main)
---
+++ ssg/site.py
@@ -0,0 +1,19 @@
+from genericpath import isdir
+from pathlib import Path
+
+
+class Site:
+    def __init__(self, source, dest):
+        self._source = Path()
+        self._dest = Path()
+
+    def create_dir(self, path):
+        directory = self._dest / Path.relative_to(self._source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self._dest.mkdir(parents=True, exist_ok=True)
+
+        for path in self._source.rglob("*"):
+            if path.isdir():
+                self.create_dir(path)
